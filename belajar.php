411dfe053c4b5e43ffb3e3133421e242816f881c
0007rosalinda <gadiselisihombing@gmail.com>
2023-04-18 15:24:50 +0700

    Komit Pertama

---
+++ belajar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$umur = 18;
+//example syntax if
+if  ($umur >= 18) {
+    echo "Anda Sudah Dewasa.<br>";
+}
+echo "rosalinda<br>" ;
+echo "221510007";
+?>
+</body>
+</html>
---
+++ strlen.php
@@ -0,0 +1,8 @@
+<?php
+echo strlen("Hello World!") . "<br>"; // outputs 12
+echo str_word_count ("Hello Word!") . "<br>"; //outputs 2
+echo strrev("Hello Wordl!") . "<br>"; //outputs !droW olleH
+echo strpos ("Hello World!", "World") . "<br>"; //outputs 6
+echo str_replace("world", "Dolly", "Hello world!"); //outputs Hello Dolly!
+
+?>
